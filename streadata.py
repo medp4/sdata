6560c3809a1c37d71ab807de2c6819ab5e632add
Med <137172388+medp4@users.noreply.github.com>
2023-11-01 20:04:02 +0100

    Update streadata.py

--- streadata.py
+++ streadata.py
@@ -11,7 +11,7 @@ def load_data(github_url):
 
 # Main function
 def main():
-    st.title("Projet avec M. Tahani Abdelouahad")
+    st.title("Projet avec M. Merimi jaouad")
 
     try:
         data = load_data(csv_github_url)
